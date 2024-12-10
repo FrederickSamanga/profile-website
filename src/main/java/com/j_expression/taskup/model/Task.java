bede448a528c3b25a3b149ad73eba4b336a67c3a
FrederickSamanga <fredericktsamanga@gmail.com>
2024-12-10 23:12:07 +0200

    "Moving to Ubuntu"

--- src/main/java/com/j_expression/taskup/model/Task.java
+++ src/main/java/com/j_expression/taskup/model/Task.java
@@ -1,16 +1,14 @@
 package com.j_expression.taskup.model;
 
-import java.time.LocalDate;
+import com.j_expression.taskup.model.TaskStatus;
+import jakarta.persistence.*;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import java.time.LocalDate;
 
 @Entity
 public class Task {
-    
-    @id
+
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -19,8 +17,67 @@ public class Task {
 
     private LocalDate dueDate;
 
-    @Enumerated(EnumType.STRING);
+    @Enumerated(EnumType.STRING)
     private TaskStatus taskStatus;
 
-    
+    public Task() {
+    }
+
+    public Task(String title, String description, LocalDate dueDate, TaskStatus taskStatus) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.taskStatus = taskStatus;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public TaskStatus getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(TaskStatus taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", dueDate=" + dueDate +
+                ", taskStatus=" + taskStatus +
+                '}';
+    }
 }
---
+++ src/main/java/com/j_expression/taskup/model/TaskStatus.java
@@ -0,0 +1,8 @@
+package com.j_expression.taskup.model;
+
+public enum TaskStatus {
+        PENDING,
+        IN_PROGRESS,
+        COMPLETED
+    }
+
